3a521b34cc7990d987976713bdaab50ba8a3ed96
Kosovomba <santiagojavierlevy@gmail.com>
2023-07-09 23:29:31 -0300

    changed a for Link on navBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import styles from '../styles/navBar.module.css'
 
 export default function NavBar() {
     return (
         <div className={styles.container}>
-            <a href="/">Inicio</a>
+            <Link href="/">Inicio</Link>
+            {/* <a href="/">Inicio</a> */}
             <button style={{width:'fit-content', margin: 10, padding: 10}}>Iniciar sesión</button>
         </div>
     )
