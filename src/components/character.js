64785dd503fe9d8f16d3959a5fc3b49ee8a1c8bc
Kosovomba <santiagojavierlevy@gmail.com>
2023-08-02 08:02:12 -0300

    ajuste en disabled de guardarpersonaje

--- src/components/character.js
+++ src/components/character.js
@@ -165,7 +165,7 @@ export default function Character ({raza, clase, nombre, razaStats, claseStats, 
             <div style={{display:'flex', flexDirection:'column'}}>
             <img style={{maxWidth: 400, maxHeight: 400, marginRight: '5px', marginLeft: '5px', border: 'ridge #754421 7px'}} width="400" src={images[`${raza}${clase}`]} alt='imagen'/>
             <div>
-            <button style={{maxWidth:'fit-content', margin: '5px'}} onClick={guardarPersonaje} disabled={usu!==''?false:true} >Guardar personaje</button>
+            <button style={{maxWidth:'fit-content', margin: '5px'}} onClick={guardarPersonaje} disabled={usu==='' || (personaje.nivel === 1 && personaje.apt1[2].length === 0 && Object.keys(personaje.CDP).length === 0 && personaje['apt2+'].length === 0)?true:false} >Guardar personaje</button>
             <button style={{maxWidth:'fit-content', margin: '5px', position:'relative', left:'180px'}} onClick={subirNivel} disabled={(personaje.nivel !== personaje['apt2+'].length + 1 || personaje.nivel>4)?true:false} >Subir de nivel</button>
             </div>
             </div>
