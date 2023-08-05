1e031ab683db0410061d69545375023746a0a020
Kosovomba <santiagojavierlevy@gmail.com>
2023-08-05 02:06:10 -0300

    added hide/show passwords on all forms

--- src/components/iniciarSesion.js
+++ src/components/iniciarSesion.js
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import {GiTireIronCross} from "react-icons/gi"
+import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai"
 
 export default function IniciarSesion({usuarios, userNames, userEMails, show, setShow}) {
     const [user, setUser] = useState({name: '', password: ''})
+    const [showPass, setShowPass] = useState(false)
     const router = useRouter()
 
     function iniciarSes() {
@@ -53,14 +55,21 @@ export default function IniciarSesion({usuarios, userNames, userEMails, show, se
         e.preventDefault()
         setShow(!show)
     }
+    function onShowPass(e) {
+        e.preventDefault()
+        setShowPass(!showPass)
+    }
 
     return (
         <div style={{display: 'grid', position: 'fixed', top: '6.5vh', width:'100%', height:'100%', background:'rgba(0, 0, 0, 0.6)'}}>
         <div style={{marginBottom: '350px', display: 'flex', flexDirection:'column', alignItems: 'center', background: 'white', minWidth:'fit-content', justifySelf:'center', padding:'20px', boxShadow:'4px 2px 2px black', width:'80%', maxWidth: '60vh', height:'fit-content', alignSelf:'center'}}>
             <button onClick={handleClose} style={{color: 'black', fontSize:'25px', border: 'solid 1px black', width:'40px', height: '40px', marginBottom: '0px',padding: '0px', position: 'absolute', alignSelf:'flex-end', justifyContent: 'center', alignItems: 'center', display: 'flex'}}><GiTireIronCross/></button>
             <form style={{display: 'flex', flexDirection:'column'}} onSubmit={(e)=> onSubmit(e)}>                                
-                <input style={{minHeight:'30px', minWidth:'350px', fontSize:'20px', padding:'6px', marginTop:'55px'}} type="text" name='name' onChange={onInputChange} value={user.name} placeholder='Nombre de usuario...'></input>
-                <input style={{minHeight:'30px', minWidth:'350px', fontSize:'20px', padding:'6px', marginTop:'25px'}} type="text" name='password' onChange={onInputChange} value={user.password} placeholder='Password...'></input>                
+                <input style={{minHeight:'30px', minWidth:'350px', fontSize:'20px', padding:'6px', marginTop:'55px', marginBottom: '20px'}} type="text" name='name' onChange={onInputChange} value={user.name} placeholder='Nombre de usuario...'></input>
+                <div style={{display:'flex', flexFlow:'row', height:'fit-content', alignItems:'center'}}>
+                <input style={{minHeight:'30px', minWidth:'350px', fontSize:'20px', padding:'6px', marginTop:'20px', marginBottom: '20px'}} type={showPass?"text":"password"} name='password' onChange={onInputChange} value={user.password} placeholder='Password...'></input>
+                <button style={{color: 'black', fontSize:'25px', border: 'solid 1px black', width:'40px', height: '40px', marginBottom: '0px', padding: '0px', justifyContent: 'center', alignItems: 'center', display: 'flex'}} onClick={onShowPass}>{showPass?<AiOutlineEye/>:<AiOutlineEyeInvisible/>}</button>
+                </div>                
                 <button style={{minHeight:'30px', minWidth:'350px', fontSize:'25px', padding:'6px', marginTop:'25px'}} type="submit" disabled={user.name === '' || user.password === ''?true:false}>Iniciar sesión</button>                
             </form>
             {router.pathname !== '/app/createUser'?<button onClick={handleClick} style={{fontSize:25, fontWeight: 600, justifyContent: 'center', width:'fit-content', marginTop: '45px'}}>Crear usuario</button>:null}
--- src/pages/app/createUser.js
+++ src/pages/app/createUser.js
@@ -2,31 +2,39 @@
 import { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
+import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai"
 import '@picocss/pico'
 export default function CreateUser() {
     
     let usuarios, userNames, userEMails
-    const [newUser, setNewUser] = useState({name: '', eMail: '', avatar: '', password: '', rePassword: ''})    
+    const [newUser, setNewUser] = useState({name: '', eMail: '', avatar: '', password: '', rePassword: ''})
+    const [showPass, setShowPass] = useState(false)
+    const [showRePass, setShowRePass] = useState(false)
+    function onShowPass(e) {
+        e.preventDefault()
+        setShowPass(!showPass)
+    }
+    function onShowRePass(e) {
+        e.preventDefault()
+        setShowRePass(!showRePass)
+    }
     axios.get('https://portfoliokoso.vercel.app/api/get-users')
     // axios.get('http://localhost:3000/api/get-users')    
         .then((users) => {
                 usuarios = users.data.usuarios
                 userNames = users.data.userNames
                 userEMails = users.data.userEMails.map(e => e.toLowerCase())
-                console.log(userNames, userEMails)
         })
         .catch((error)=> {
             console.log(error)
         })
     const router = useRouter()
-    console.log(router.pathname)
     let errorName = false, errorEMail = false, errorPassword = false, errorRePassword = false
     
     
     function onInputChange(e) {
         e.preventDefault()        
         setNewUser({...newUser, [e.target.name]: e.target.value})
-        console.log(newUser)
     }
 
     function onSubmit(e) {
@@ -68,8 +76,16 @@ export default function CreateUser() {
             <form onSubmit={(e)=> onSubmit(e)} style={{width: '400px', marginTop:'12vh', maxHeight:'fit-content', padding: '7px'}}>                
                 <input type="text" name='eMail' onChange={onInputChange} value={newUser.eMail} placeholder='Correo electrónico...'></input>
                 <input type="text" name='name' onChange={onInputChange} value={newUser.nombre} placeholder='Nombre de usuario... (hasta 15 caracteres alfanuméricos)'></input>
-                <input type="text" name='password' onChange={onInputChange} value={newUser.password} placeholder='Password... (hasta 15 caracteres alfanuméricos)'></input>
-                <input type="text" name='rePassword' onChange={onInputChange} value={newUser.rePassword} placeholder='Repite el password...'></input>
+                {/* <input type="text" name='password' onChange={onInputChange} value={newUser.password} placeholder='Password... (hasta 15 caracteres alfanuméricos)'></input> */}
+                <div style={{display:'flex', flexFlow:'row', height:'fit-content', alignItems:'center'}}>
+                <input type={showPass?"text":"password"} name='password' onChange={onInputChange} value={newUser.password} placeholder='Password... (hasta 15 caracteres alfanuméricos)'></input>
+                <button style={{color: 'black', fontSize:'25px', border: 'solid 1px black', width:'40px', height: '40px', marginBottom: '0px', padding: '0px', justifyContent: 'center', alignItems: 'center', display: 'flex'}} onClick={onShowPass}>{showPass?<AiOutlineEye/>:<AiOutlineEyeInvisible/>}</button>
+                </div>
+                {/* <input type="text" name='rePassword' onChange={onInputChange} value={newUser.rePassword} placeholder='Repite el password...'></input> */}
+                <div style={{display:'flex', flexFlow:'row', height:'fit-content', alignItems:'center'}}>
+                <input type={showRePass?"text":"password"} name='rePassword' onChange={onInputChange} value={newUser.rePassword} placeholder='Repite el password...'></input>
+                <button style={{color: 'black', fontSize:'25px', border: 'solid 1px black', width:'40px', height: '40px', marginBottom: '0px', padding: '0px', justifyContent: 'center', alignItems: 'center', display: 'flex'}} onClick={onShowRePass}>{showRePass?<AiOutlineEye/>:<AiOutlineEyeInvisible/>}</button>
+                </div>
                 <button type="submit" disabled={errorName === false && errorEMail === false && errorPassword === false && errorRePassword === false?false:true}>Crear usuario</button>
                 {/* <button type="submit">Crear usuario</button> */}
             </form>
